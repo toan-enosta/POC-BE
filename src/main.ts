ae4e7061256ed842369f2390714415dd6fcf46e4
DANGCONGTOAN <dangcongtoan@192.168.2.111>
2024-05-19 01:56:50 +0700

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 import { DATA_FOLDER_PATH } from './constant';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
 }
 
--- src/model/Step.model.ts
+++ src/model/Step.model.ts
@@ -1,4 +1,5 @@
 export interface Step {
+  id: string;
   name: string;
   element: string;
   url: string;
